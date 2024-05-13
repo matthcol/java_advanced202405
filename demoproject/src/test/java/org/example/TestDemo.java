2d192005838be09bfe424257e740f9b94dfd33ea
matthcol <matthias.colin@gmail.com>
2024-05-13 10:42:42 +0200

    finish 1st test

--- demoproject/src/test/java/org/example/TestDemo.java
+++ demoproject/src/test/java/org/example/TestDemo.java
@@ -1,2 +1,15 @@
-package org.example;public class TestDemo {
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDemo {
+
+    @Test
+    void testJunit_Ok(){
+        var text = "Demo";
+        assertEquals("Demo", text);
+    }
 }
