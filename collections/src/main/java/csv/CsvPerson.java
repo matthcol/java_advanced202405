bee75aa432a141cc57aba1a59021b0bd546b9b13
matthcol <matthias.colin@gmail.com>
2024-05-14 09:19:51 +0200

    tu lineToInfoMap

---
+++ collections/src/main/java/csv/CsvPerson.java
@@ -0,0 +1,10 @@
+package csv;
+
+import java.util.Map;
+
+public class CsvPerson {
+    public static Map<String,Object> lineToInfoMap(String line){
+        // TODO
+        return Map.of();
+    }
+}
---
+++ collections/src/test/java/tu/csv/CsvPersonTest.java
@@ -0,0 +1,35 @@
+package tu.csv;
+
+import csv.CsvPerson;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvPersonTest {
+
+    @Test
+    void testLineToInfoMap_birthdatePresent() {
+        String line = "1\tFred Astaire\t1899-05-10";
+        var infoMap = CsvPerson.lineToInfoMap(line);
+        assertEquals(3, infoMap.size(), "info count");
+        int id = (int) infoMap.get("id");
+        String name = (String) infoMap.get("name");
+        LocalDate birthdate = (LocalDate) infoMap.get("birthdate");
+        assertEquals(1, id, "id");
+        assertEquals("Fred Astaire", name, "name");
+        assertEquals(LocalDate.of(1899,5,10), birthdate);
+    }
+
+    @Test
+    void testLineToInfoMap_birthdateAbsent() {
+        String line = "1\tFred Astaire\t";
+        var infoMap = CsvPerson.lineToInfoMap(line);
+        assertEquals(2, infoMap.size(), "info count");
+        int id = (int) infoMap.get("id");
+        String name = (String) infoMap.get("name");
+        assertEquals(1, id, "id");
+        assertEquals("Fred Astaire", name, "name");
+    }
+}
